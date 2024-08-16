70442f4c8e89e631a02bf23e0bdaa3d912380d1b
nitintandukar <nitin.tandukar0422@gmail.com>
2024-08-16 11:45:22 +0545

    Registerd Login endpoint for user login

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from account.views import *
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register_user')
+    path('register/', UserRegistrationView.as_view(), name='register_user'),
+    path('login/', UserLoginView.as_view(), name='login_user'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -5,6 +6,11 @@ from .serializers import *
 
 '''
     Endpoint for registration of users
+    Uses renderer for better representation of errors
+    Takes email, country, name, password and password2 as valid data
+    Returns 201 if registration successful 
+    else
+    Returns 400 as bad request 
 '''
 class UserRegistrationView(APIView):
     def post(self, request, format=None):
@@ -12,3 +18,23 @@ class UserRegistrationView(APIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({'message': 'Registration Successful'}, status=status.HTTP_201_CREATED)
+    
+'''
+    Endpoint for login 
+    Takes email and password as valid data
+    Returns 200 if login is successful
+    else
+    Returns 400 if data is invalid
+    else
+    Returns 404 if user with invalid credentials is provided
+'''
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.data.get('email')
+        password = serializer.data.get('password')
+        user = authenticate(email=email, password= password)
+        if user is not None:
+            return Response({'message':'Login Successful'}, status=status.HTTP_200_OK)
+        return Response({'message': 'User credentials does not match'}, status=status.HTTP_404_NOT_FOUND)
